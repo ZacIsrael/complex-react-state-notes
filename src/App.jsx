1789307255dce7ac4a00b3a02c6c89850e7d1a0a
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-15 13:10:59 -0400

    Implemented code that updates the h1 element with the user's first name and last name as they type them.

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,79 @@ function App() {
 
   const [count, setCount] = useState(0);
 
+  // varible and fucntion to manage state of the user's first name
+  const [firstName, setFirstName] = useState("");
+
+  // varible and fucntion to manage state of the user's last name
+  const [lastName, setLastName] = useState("");
+
+  const [fNameHeading, setFNameHeading] = useState("");
+
+  const [lNameHeading, setLNameHeading] = useState("");
+
+  function handleFirstNameChange (){
+
+    // variable that stores what the user has typed
+    let val = event.target.value;
+
+    // variable that stores the type of input that is expected
+    let type = event.target.type;
+
+    // variable that stores the placeholder text
+    let placeholder = event.target.placeholder;
+
+    console.log("handleFirstNameChange():  val = ", val, "type = ", type, "placeholder = ", placeholder);
+    // sets the value of the name to the value of the user's input as they type
+    setFirstName(val);
+
+  }
+
+  function handleLastNameChange (){
+
+    // variable that stores what the user has typed
+    let val = event.target.value;
+
+    // variable that stores the type of input that is expected
+    let type = event.target.type;
+
+    // variable that stores the placeholder text
+    let placeholder = event.target.placeholder;
+
+    console.log("handleLastNameChange():  val = ", val, "type = ", type, "placeholder = ", placeholder);
+    // sets the value of the name to the value of the user's input as they type
+    setLastName(val);
+  }
+
+  function handleClickSubmitBtn (){
+    console.log("handleClickSubmitBtn(): ");
+
+    // retrieve the button that was clicked
+    let btn = event.target;
+    // make the background of the button black once it's clicked
+    btn.style.backgroundColor = "black";
+
+    // turn the background back white after 0.05 seconds
+    setTimeout(() => {
+      btn.style.backgroundColor = "white";
+    }, 50);
+
+     // updates the first and last names in the headings with names that the user typed in to the input HTML elements
+     setFNameHeading(firstName);
+     setLNameHeading(lastName);
+
+     event.preventDefault();
+  }
+
   return (
     <>
       <div className="container">
-        <h1>Hello</h1>
+        <h1>Hello {firstName} {lastName}</h1>
+        {/* <h1>Hello {fNameHeading} {lNameHeading}</h1> */}
         <form>
-          <input name="fName" placeholder="First Name" />
-          <input name="lName" placeholder="Last Name" />
+          <input onChange={handleFirstNameChange} name="fName" placeholder="First Name" />
+          <input onChange={handleLastNameChange} name="lName" placeholder="Last Name" />
           <button>Submit</button>
+          {/* <button onClick={handleClickSubmitBtn}>Submit</button> */}
         </form>
       </div>
     </>
