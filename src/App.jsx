37013e533964fcf088ea940924be5139ee779a80
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-15 12:56:24 -0400

    added necessary starting code. Updated README.MD file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   // How to create a React app: npm create vite@latest {app-name} -- --template react
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      <div className="container">
+        <h1>Hello</h1>
+        <form>
+          <input name="fName" placeholder="First Name" />
+          <input name="lName" placeholder="Last Name" />
+          <button>Submit</button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
